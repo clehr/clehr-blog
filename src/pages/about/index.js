f28368a2d116f3fa9d1affa4ab86b05202cb4c47
clehr <christian_lehr.1@gmx.de>
2019-04-26 16:26:25 +0200

    add content to about page

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -25,15 +25,11 @@ const About = ({ data: { profilePhoto, skillIcons, toolIcons } }) => {
           <h1>Hi, I'm Christian!</h1>
           <h2>Software Developer</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-            cursus venenatis arcu, cursus pretium enim lacinia nec. Duis viverra
-            sagittis neque. Fusce non luctus urna. Vivamus suscipit metus ac
-            posuere egestas. Nunc a pulvinar purus. Vivamus nisi mi, fringilla
-            quis lacus et, sagittis mollis massa. Cras tempus massa quis
-            lobortis laoreet. Pellentesque metus odio, sagittis nec venenatis
-            non, maximus congue eros. Suspendisse pellentesque purus sit amet
-            ante commodo, et molestie mauris aliquet. Proin non nibh libero.
-            Fusce at nulla euismod, condimentum augue quis, convallis justo.
+            I'm a 28 years old developer who is always curious about learning new things let it be
+            programming or other things like cooking, violin or learning languages like russian, japanese.
+
+            It's my goal to find out about how to learn things efficiently and with a lot of fun and show that
+            to other people because in my opinion learning should be all about fun!
           </p>
           <br />
           <h2>Skills</h2>
