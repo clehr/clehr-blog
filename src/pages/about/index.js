2baebc5e5e32a7ae6e9d79844da054c216f1a1ed
clehr <christian_lehr.1@gmx.de>
2019-04-28 11:34:04 +0200

    add impressum

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -37,6 +37,19 @@ const About = ({ data: { profilePhoto, skillIcons, toolIcons } }) => {
           <h2>Tools</h2>
           <ImageList edges={toolIcons.edges} />
         </div>
+
+        <div className='impressum'><h1>Impressum</h1><p>Angaben gemäß § 5 TMG</p><p>Christian Lehr <br/>
+          Berliner Straße%2C 105<br/>
+          21614 Buxtehude <br/>
+        </p><p><strong>Vertreten durch: </strong><br/>
+          Christian Lehr<br/>
+        </p><p><strong>Kontakt:</strong> <br/>
+          Telefon: 0172-1587676<br/>
+          E-Mail: <a href='mailto:christian_lehr.1@gmx.de'>christian_lehr.1@gmx.de</a><br/></p><br/>
+          Impressum vom <a href="https://www.impressum-generator.de">Impressum Generator</a> der <a
+            href="https://www.kanzlei-hasselbach.de/">Kanzlei Hasselbach, Rechtsanwälte für Arbeitsrecht und
+            Familienrecht</a>
+        </div>
       </div>
     </Layout>
   )
